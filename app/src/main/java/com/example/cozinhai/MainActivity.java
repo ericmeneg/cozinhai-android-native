c95cf4e3c4fcd4c84db6e2efaab1446281652c31
grazisouza1 <graziisouza0127@gmail.com>
2026-05-19 10:44:41 -0300

    feat: Adding components by id

--- app/src/main/java/com/example/cozinhai/MainActivity.java
+++ app/src/main/java/com/example/cozinhai/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.text.Html;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.main_activity);
-
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+
+        ImageView mainLogo = findViewById(R.id.mainLogo);
+        TextView bemVindoText = findViewById(R.id.bemVindoText);
+        TextView entreText = findViewById(R.id.entreText);
+        EditText emailInput = findViewById(R.id.emailInput);
+        EditText passwordInput = findViewById(R.id.passwordInput);
+        Button entrarBtn = findViewById(R.id.entrarBtn);
+        TextView semContaText = findViewById(R.id.semContaText);
+        TextView cadastroText = findViewById(R.id.cadastroText);
+        TextView visitanteOption = findViewById(R.id.visitanteOption);
+
+        cadastroText.setText(
+                Html.fromHtml(getString(R.string.link_cadastro))
+        );
+
     }
 }
