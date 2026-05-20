78244673eb62f2f058b096b5582f4b0fe0a2e1a3
ericmeneg <ericvinicius.menegon@gmail.com>
2026-05-20 11:09:09 -0300

    feat: Adiciona conectividade com API de autenticação a tela de login
    - faz chamada a API
    
    - divide a lógica em diferentes galhos dependendo da resposta da API

--- app/src/main/java/com/example/cozinhai/MainActivity.java
+++ app/src/main/java/com/example/cozinhai/MainActivity.java
@@ -6,13 +6,21 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import android.text.Html;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String BASE_URL = "https://pi-3sem-backend.onrender.com/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +37,44 @@ public class MainActivity extends AppCompatActivity {
         TextView semContaText = findViewById(R.id.semContaText);
         TextView cadastroText = findViewById(R.id.cadastroText);
         TextView visitanteOption = findViewById(R.id.visitanteOption);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        AuthApi authApi = retrofit.create(AuthApi.class);
+
+        entrarBtn.setOnClickListener(view -> {
+            String email = emailInput.getText().toString().trim();
+            String password = passwordInput.getText().toString().trim();
+
+            if (email.isEmpty() || password.isEmpty() ){
+                Log.d("LOGIN", "Campo email ou senha vazio(s)");
+                return;
+            }
+
+            LoginRequest request = new LoginRequest(email, password);
+
+            authApi.login(request).enqueue(new Callback<Object>(){
+                @Override
+                public void onResponse(
+                        Call<Object> call,
+                        Response<Object> response
+                ) {
+                    if (response.isSuccessful()) {
+                        Log.d("LOGIN", "Sucesso: " + response.body());
+                    } else {
+                        Log.d("LOGIN", "Código de erro: " + response.code());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Object> call, Throwable t) {
+                    Log.e("LOGIN", "Request falhou", t);
+                }
+            });
+        });
 
         cadastroText.setText(
                 Html.fromHtml(getString(R.string.link_cadastro))
