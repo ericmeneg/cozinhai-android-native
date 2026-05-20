6bc3681f5d5f479195c103d54722bd761e2d9f64
grazisouza1 <graziisouza0127@gmail.com>
2026-05-20 11:12:40 -0300

    feat: Starting home screen

---
+++ app/src/main/java/com/example/cozinhai/Home.java
@@ -0,0 +1,22 @@
+package com.example.cozinhai;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+
+    }
+}
