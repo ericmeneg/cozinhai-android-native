8861771ce148dc7a2834da0eae3634e6fba6056c
Seu Nome <robsonamantovani@gmail.com>
2026-06-11 09:33:00 -0300

    Correção na apresentação da receita dos favoritos

--- app/src/main/java/com/example/cozinhai/Recipe.java
+++ app/src/main/java/com/example/cozinhai/Recipe.java
@@ -3,7 +3,9 @@ package com.example.cozinhai;
 import com.google.gson.annotations.SerializedName;
 
 public class Recipe {
+    @SerializedName(value = "id", alternate = {"recipeId"})
     private int id;
+
     private String title;
 
     @SerializedName(value = "image", alternate = {"recipeImage"})
